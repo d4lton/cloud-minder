3cb0e338fdbadab15fdfb5bc8cd5aea3130c8bb1
Dana Basken <dbasken@gmail.com>
2020-08-27 16:18:57 +0000

    added 'npm install' to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 NODE=`which node`
 
+npm install
+
 cat cloud-minder.service.template | sed s+__USER__+$USER+ | sed s+__NODE__+$NODE+ > cloud-minder.service
 
 sudo cp cloud-minder.service /etc/systemd/system/
